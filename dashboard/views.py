2312beb438887affd2e45e46b6819c3d6c4eb35d
Brieuc Doucy <2885025d@student.gla.ac.uk>
2025-03-09 00:28:48 +0000

    basic Url mapping and base html creation

---
+++ dashboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'dashboard/index.html')
+def meetings(request):
+    return render(request,'dashboard/meetings.html')
+def create_meeting(request):
+    return render(request,'dashboard/create_meeting.html')
+def notifications(request):
+    return render(request,'dashboard/notifications.html')
+
--- eventio/views.py
+++ eventio/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+def contact_us(request):
+    return render(request,'contact_us.html')
+def about(request):
+    return render(request,'about.html')
+def login(request):
+	return render(request, 'login.html')
+def register(request):
+	return render(request, 'register.html')
