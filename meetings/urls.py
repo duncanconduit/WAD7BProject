be542b5063c4e62a82ea2a0f905929a4b058c89c
Robbie Copeland <2768763c@student.gla.ac.uk>
2025-03-21 13:35:45 +0000

    meetings pages added, urls for these added too, views not done yet

--- meetings/urls.py
+++ meetings/urls.py
@@ -3,8 +3,9 @@ from meetings import views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('',  RedirectView.as_view(url='/accounts/meetings/', permanent=True), name='meetings'),
+    path('', RedirectView.as_view(url='/accounts/meetings/', permanent=True), name='meetings'),
     path('meeting/<int:meeting_id>/', views.meeting_detail_view, name='view'),
+    path('meeting/<int:meeting_id>/edit/', views.edit_meeting, name='edit_meeting'),
     path('create/', views.create_meeting, name='create_meeting'),
     path('calendar/', views.calendar_view, name='calendar'),
     path('api/meetings/', views.get_meetings, name='get_meetings'),
