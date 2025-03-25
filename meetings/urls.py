ffbc3758eab2f00913bbfb2b22d15b0bdc78ea57
Robbie Copeland <2768763c@student.gla.ac.uk>
2025-03-25 22:33:14 +0000

    meetings tests added, extra problems fixed to do with meetings

--- meetings/urls.py
+++ meetings/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from meetings import views
 from django.views.generic.base import RedirectView
 
+app_name = 'meetings' 
 urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/meetings/', permanent=True), name='meetings'),
-    path('meeting/<int:meeting_id>/', views.meeting_detail_view, name='view'),
-    path('meeting/<int:meeting_id>/edit/', views.edit_meeting, name='edit_meeting'),
+    path('meetings/meeting/<uuid:meeting_id>/edit/', views.edit_meeting, name='edit_meeting'),
+    path('meetings/meeting/<uuid:meeting_id>/', views.meeting_view, name='view_meeting'),  
     path('create/', views.create_meeting, name='create_meeting'),
     path('calendar/', views.calendar_view, name='calendar'),
     path('api/meetings/', views.get_meetings, name='get_meetings'),
